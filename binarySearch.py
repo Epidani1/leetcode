bf86e75c6c776da9ad25811cb741a8e0d2303992
joshjpark <joshua-explorer@hotmail.com>
2020-01-30 22:28:19 -0800

    Q704

---
+++ binarySearch.py
@@ -0,0 +1,25 @@
+''' 
+Question 704: Binary Search: Easy 
+URL: https://leetcode.com/problems/binary-search/
+'''
+class Solution:
+    def search(self, nums, target):
+        low, high = 0, len(nums)-1
+        if target > nums[high] or target < nums[low]:
+            return -1
+
+        while (low <= high):
+            curr = (low + high) // 2            
+            if nums[curr] == target:
+                return curr
+            if nums[curr] > target:
+                high = curr-1
+            elif nums[curr] < target:
+                low = curr+1
+        return -1
+
+
+
+sn = Solution()
+# print(sn.search([-1,0,3,5,9,12], 9))
+print(sn.search([2,5], 5))
